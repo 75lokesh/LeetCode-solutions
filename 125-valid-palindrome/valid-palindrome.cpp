12d009f8f72b81ed13d94e005a154c64975d4bc8
Lokesh Lodhi IITD <lokeshiit2022@gmail.com>
2026-09-13 01:23:25 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (7.68%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,27 +1,47 @@
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         int left = 0;
+//         int right = s.length() - 1;
+
+//         while (left < right) {
+//             while (left < right && !isalnum((unsigned char)s[left])) {
+//                 left++;
+//             }
+
+//             while (left < right && !isalnum((unsigned char)s[right])) {
+//                 right--;
+//             }
+
+//             if (tolower((unsigned char)s[left]) !=
+//                 tolower((unsigned char)s[right])) {
+//                 return false;
+//             }
+
+//             left++;
+//             right--;
+//         }
+
+//         return true;
+//     }
+// };
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int left = 0;
-        int right = s.length() - 1;
+        string temp = "";
 
-        while (left < right) {
-            while (left < right && !isalnum((unsigned char)s[left])) {
-                left++;
+        // Remove non-alphanumeric and convert to lowercase
+        for (char c : s) {
+            if (isalnum(c)) {
+                temp += tolower(c);
             }
-
-            while (left < right && !isalnum((unsigned char)s[right])) {
-                right--;
-            }
-
-            if (tolower((unsigned char)s[left]) !=
-                tolower((unsigned char)s[right])) {
-                return false;
-            }
-
-            left++;
-            right--;
         }
 
-        return true;
+        // Create reverse
+        string rev = temp;
+        reverse(rev.begin(), rev.end());
+
+        // Compare
+        return temp == rev;
     }
 };
